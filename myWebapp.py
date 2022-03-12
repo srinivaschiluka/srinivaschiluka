d1f7bed988b710df443f7cbab0387b2a841e7d5f
srinivaschiluka <101299309+srinivaschiluka@users.noreply.github.com>
2022-03-11 23:19:19 -0500

    Update myWebapp.py

--- myWebapp.py
+++ myWebapp.py
@@ -10,5 +10,5 @@ tickerdata = yf.Ticker(ts)
 tickerdf= tickerdata.history(period='1d',start='2022-03-01',end='2022-03-10')
 st.line_chart(tickerdf.Close)
 st.line_chart(tickerdf.Volume)
-tickerdf
+
 

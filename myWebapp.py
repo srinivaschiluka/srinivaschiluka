c0179aede10a93d0b3725113e22957b7cf80d579
srinivaschiluka <101299309+srinivaschiluka@users.noreply.github.com>
2022-03-11 22:06:19 -0500

    Update myWebapp.py

--- myWebapp.py
+++ myWebapp.py
@@ -1,11 +1,5 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
-import streamlit as st
-st.write ("""
-#srinivas
-Chiluka
-""")
-
 ts='MSFT'
 tickerdata = yf.Ticker(ts)
 tickerdf= tickerdata.history(period='1d',start='2022-03-01',end='2022-03-10')

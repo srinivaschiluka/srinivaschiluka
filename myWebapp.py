f655f5381c493e2bdcf224d866b7dafd9ac308e1
srinivaschiluka <101299309+srinivaschiluka@users.noreply.github.com>
2022-03-11 21:36:46 -0500

    Update myWebapp.py

--- myWebapp.py
+++ myWebapp.py
@@ -1,1 +1,7 @@
 import yfinance as yf
+import matplotlib.pyplot as plt
+ts='MSFT'
+tickerdata = yf.Ticker(ts)
+tickerdf= tickerdata.history(period='1d',start='2022-03-01',end='2022-03-10')
+data=tickerdf.Close
+plt.plot(data)
